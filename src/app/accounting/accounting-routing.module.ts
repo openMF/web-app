052568bba6d5590aa673b7fee6bbbeb890b0caf1
Abhay Chawla <abhay.chawla97@gmail.com>
2018-06-15 00:21:22 +0530

    fix: multiple usage of app header (#48)
    Remove multiple usage of app header component and use the shell component
    to route with shell, loading the header only once initially.
    
    Fixes #47

--- src/app/accounting/accounting-routing.module.ts
+++ src/app/accounting/accounting-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { Route, extract } from '@app/core';
 import { AccountingComponent } from './accounting.component';
 
 const routes: Routes = [
-  { path: 'accounting', component: AccountingComponent},
+  Route.withShell([
+    { path: 'accounting', component: AccountingComponent}
+  ])
 ];
 
 @NgModule({
