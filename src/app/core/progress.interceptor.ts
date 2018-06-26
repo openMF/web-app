32b94503dd60c201685f20c015736ae5fb7012e4
Abhay Chawla <abhay.chawla97@gmail.com>
2018-06-26 17:01:43 +0530

    feat: add progress bar (#66)
    Add progress bar for loading data via http requests.
    
    Fixes #65

---
+++ src/app/core/progress.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { ProgressBarService } from './progress-bar.service';
+
+@Injectable()
+export class ProgressInterceptor implements HttpInterceptor {
+
+  constructor(private progressBarService: ProgressBarService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.progressBarService.increase();
+    return next
+      .handle(request).pipe(
+        tap(event => {
+          if (event instanceof HttpResponse) {
+            this.progressBarService.decrease();
+          }
+        })
+      );
+  }
+
+}
