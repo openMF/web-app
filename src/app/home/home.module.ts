0ee702be485e243baaaa07005e4a8cca70c5840b
abhay_1806 <abhay.chawla97@gmail.com>
2018-06-28 22:43:32 +0530

    feat: add custom themes
    Add custom themes based on logo of Mifos
    
    Fixes #73

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
+import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -13,6 +14,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   imports: [
     CommonModule,
+    CoreModule,
     TranslateModule,
     NgxChartsModule,
     SharedModule,
