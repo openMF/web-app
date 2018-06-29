debc22767c06a7a23b05f26f75f19a7d2bb9c11e
anweshknayak <anweshknayak@gmail.com>
2018-06-30 00:58:53 +0530

    routerlink table

--- src/app/clients/view-client/view-client.component.ts
+++ src/app/clients/view-client/view-client.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'mifosx-view-client',
   templateUrl: './view-client.component.html',
   styleUrls: ['./view-client.component.scss']
 })
-export class ViewClientComponent implements OnInit {
+export class ViewClientComponent implements OnInit, OnDestroy {
+  id: number = null;
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.paramsSubscription = this.route.params
+    .subscribe(
+      (params: Params) => {
+        this.id = params['id'];
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
 }
