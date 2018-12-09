111953a8255603d24663e51866a243e396f14265
punwai <punwai@yahoo.com>
2018-12-09 11:06:05 +0000

    feat: add scheduler jobs component

---
+++ src/app/system/manage-scheduler-jobs/manage-scheduler-jobs.resolver.ts
@@ -0,0 +1,33 @@
+/** Angular Imports */
+import { Injectable } from '@angular/core';
+import { Resolve } from '@angular/router';
+
+/** rxjs Imports */
+import { Observable, forkJoin } from 'rxjs';
+
+/** Custom Services */
+import { SystemService } from '../system.service';
+
+/**
+ * Manage Scheduler Jobs data resolver.
+ */
+@Injectable()
+export class ManageSchedulerJobsResolver implements Resolve<Object> {
+
+  /**
+   * @param {SystemService} systemService System service.
+   */
+  constructor(private systemService: SystemService) {}
+
+  /**
+   * Returns the manage scheduler jobs data.
+   * @returns {Observable<any>}
+   */
+  resolve() {
+    return forkJoin(
+      this.systemService.getJobs(),
+      this.systemService.getScheduler()
+    );
+  }
+
+}
