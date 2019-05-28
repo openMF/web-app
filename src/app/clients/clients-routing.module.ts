d2bbdfca7426ce4b13499762a9b228d0e6a80614
Jivjyot Singh <jivjyotsingh@gmail.com>
2019-05-28 18:07:12 +0530

    refactor: Clients Module
    - refactors clients-routing.module.ts for dynamic breadcrumbs.
    - refactors clients.service.ts to use HttpClient
    - adds resolver to load client data
    - removes partially implemented components
    
    fixes: #409

--- src/app/clients/clients-routing.module.ts
+++ src/app/clients/clients-routing.module.ts
@@ -7,63 +7,30 @@ import { Route } from '../core/route/route.service';
 /** Translation Imports */
 import { extract } from '../core/i18n/i18n.service';
 
+/** Custom Components */
 import { ClientsComponent } from './clients.component';
-import { CreateClientComponent} from './create-client/create-client.component';
-import { ViewClientComponent } from './view-client/view-client.component';
-import { ViewLoanComponent } from './view-loan/view-loan.component';
-import { CreateAddressComponent } from './create-address/create-address.component';
-import { CreateIdentityComponent } from './create-identity/create-identity.component';
-import { UploadDocumentComponent } from './upload-document/upload-document.component';
-import { EditAddressComponent } from './edit-address/edit-address.component';
+
+/** Custom Resolvers */
+import { ClientsResolver } from './common-resolvers/clients.resolver';
 
 const routes: Routes = [
   Route.withShell([
     {
       path: 'clients',
       component: ClientsComponent,
-      data: { title: extract('Clients') }
-    },
-    {
-      path: 'clients/create',
-      component: CreateClientComponent,
-      data: { title: extract('Create Client') }
-    },
-    {
-      path: 'clients/view/:id',
-      component: ViewClientComponent,
-      data: { title: extract('View Client') }
-    },
-    {
-      path: 'clients/viewloan',
-      component: ViewLoanComponent,
-      data: { title: extract('View Loan') }
-    },
-    {
-      path: 'clients/address/:id',
-      component: CreateAddressComponent,
-      data: { title: extract('Create Address') }
-    },
-    {
-      path: 'clients/editaddress/:id/:addressTypeId/:addressId',
-      component: EditAddressComponent,
-      data: { title: extract('Edit Address') }
-    },
-    {
-      path: 'clients/addclientidentifier/:id',
-      component: CreateIdentityComponent,
-      data: { title: extract('Create Identity') }
-    },
-    {
-      path: 'clients/addclientdocument/:id',
-      component: UploadDocumentComponent,
-      data: { title: extract('Create Identity') }
-    },
+      data: { title: extract('Clients'), breadcrumb: 'Clients', routeParamBreadcrumb: false },
+      resolve: {
+        clientsData: ClientsResolver
+      }
+    }
   ])
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: []
+  providers: [
+    ClientsResolver
+  ]
 })
 export class ClientsRoutingModule { }
