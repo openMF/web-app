f5aac68888048cd44b1af05be65610a4afe8d6a7
Jivjyot Singh <jivjyotsingh@gmail.com>
2019-06-16 22:13:59 +0530

    fix: production build on gh-pages
    Fixes: #416

--- src/app/pipes/fetch-image.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FetchImagePipe } from './fetch-image.pipe';
-
-describe('FetchImagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FetchImagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
