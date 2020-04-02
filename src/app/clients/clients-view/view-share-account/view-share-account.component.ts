8f542f502760275ed825e6ffff29d3d524799dd8
Mutahar Majid <mutaharmajid@Mutahars-MacBook-Air.local>
2020-04-02 17:42:30 +0530

    refactor: refactor externalId code and add comment lines
    add new code to display externalId of a client and also add comment lines to make tab label arrays  more understandable.
    
    Fixes: #711
    See also: #231

--- src/app/clients/clients-view/view-share-account/view-share-account.component.ts
+++ src/app/clients/clients-view/view-share-account/view-share-account.component.ts
@@ -7,20 +7,23 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./view-share-account.component.scss']
 })
 export class ViewShareAccountComponent implements OnInit {
+  // Charges tab Columns Labels
   chargesColumns: string[] = ['Name', 'Fee/Penalty', 'Payment due at', 'Calculation Type', 'Due', 'Paid', 'Waived', 'Outstanding'];
+  // Transaction Overview tab Column Labels
   transactionColumns: string[] = ['Transaction Date', 'Transaction Type',	'Total Shares', 'Purchased/Redeemed Price', 'Charge Amount', 'Amount Received/Returned'];
+  // Dividend Tab Column Labels
   dividendColumns: string[] = ['Transaction Date', 'Amount', 'Transaction Reference', 'Status'];
+  // Client share-account data variable
   shareAccount: any;
 
   constructor(
     private route: ActivatedRoute,
-  ) {
-    this.route.data.subscribe((data: {shareAccountsData: any}) => {
-      this.shareAccount = data.shareAccountsData;
-    });
-   }
+  ) {}
 
   ngOnInit() {
+    this.route.data.subscribe((data: {shareAccountsData: any, }) => {
+      this.shareAccount = data.shareAccountsData;
+    });
   }
 
 }
