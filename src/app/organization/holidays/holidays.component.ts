e3d5bd81569ffcb5c848b75982f3e16231e7ce03
Rishabh Verma <rishabhgreat106@gmail.com>
2020-05-11 15:25:30 +0530

    feat: add holidays component (#730)
    Fixes: #153

---
+++ src/app/organization/holidays/holidays.component.ts
@@ -0,0 +1,89 @@
+/** Angular Imports */
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl } from '@angular/forms';
+
+/** rxjs Imports */
+import { of } from 'rxjs';
+
+/** Custom Services */
+import { OrganizationService } from '../organization.service';
+
+/**
+ * Holidays component.
+ */
+@Component({
+  selector: 'mifosx-holidays',
+  templateUrl: './holidays.component.html',
+  styleUrls: ['./holidays.component.scss']
+})
+export class HolidaysComponent implements OnInit {
+
+  /** Office selector. */
+  officeSelector = new FormControl();
+  /** Holidays data. */
+  holidaysData: any;
+  /** Offices data. */
+  officeData: any;
+  /** Columns to be displayed in holidays table. */
+  displayedColumns: string[] = ['name', 'fromDate', 'toDate', 'repaymentsRescheduledTo', 'status'];
+  /** Data source for holidays table. */
+  dataSource: MatTableDataSource<any>;
+
+  /** Paginator for holidays table. */
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  /** Sorter for holidays table. */
+  @ViewChild(MatSort) sort: MatSort;
+
+  /**
+   * Retrieves the offices data from `resolve`.
+   * @param {OrganizationService} organizationService Organization Service.
+   * @param {ActivatedRoute} route Activated Route.
+   * @param {FormBuilder} formBuilder Form Builder.
+   */
+  constructor(private organizationService: OrganizationService,
+              private route: ActivatedRoute) {
+    this.route.data.subscribe(( data: { offices: any }) => {
+      this.officeData = data.offices;
+    });
+  }
+
+  /**
+   * Filters data in holidays table based on passed value.
+   * @param {string} filterValue Value to filter data.
+   */
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  /**
+   * Retrieves holidays data on changing office.
+   */
+  ngOnInit() {
+    this.onChangeOffice();
+  }
+
+  /**
+   * Retrieves the holidays data on changing office and sets the holidays table.
+   */
+  onChangeOffice() {
+    this.officeSelector.valueChanges.subscribe((officeId = this.officeSelector.value) => {
+      this.holidaysData = [];
+      this.organizationService.getHolidays(officeId).subscribe((holidays: any) => {
+        this.holidaysData = holidays;
+        this.setHolidays();
+      });
+    });
+  }
+
+  /**
+   * Initializes the data source, paginator and sorter for holidays table.
+   */
+  setHolidays() {
+    this.dataSource = new MatTableDataSource(this.holidaysData);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+}
