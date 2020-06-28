1c784f69e4be11ed4549cdfd63f70b005536b326
karan <karant@iitk.ac.in>
2020-06-29 00:53:38 +0530

    feat; savings account actions (enable/disable-withhold-tax)
    Fixes: #944

---
+++ src/app/savings/savings-account-view/custom-dialogs/toggle-withhold-tax-dialog/toggle-withhold-tax-dialog.component.ts
@@ -0,0 +1,22 @@
+/** Angular Imports */
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+/**
+ * Toggle withhold tax dialog dialog component.
+ */
+@Component({
+  selector: 'mifosx-toggle-withhold-tax-dialog',
+  templateUrl: './toggle-withhold-tax-dialog.component.html',
+  styleUrls: ['./toggle-withhold-tax-dialog.component.scss']
+})
+export class ToggleWithholdTaxDialogComponent {
+
+  /**
+   * @param {MatDialogRef} dialogRef Component reference to dialog.
+   * @param {any} data.
+   */
+  constructor(public dialogRef: MatDialogRef<ToggleWithholdTaxDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+}
