380d09c22e6b47e85edaed5b1fa8c55cbc80cb57
Karan Takalkar <karant@iitk.ac.in>
2020-07-04 18:07:23 +0530

    feat: fixed deposit account charges actions (#991)
    Fixes: 990

---
+++ src/app/deposits/fixed-deposits/fixed-deposit-account-view/view-transaction/view-transaction.component.ts
@@ -0,0 +1,32 @@
+/** Angular Imports */
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material';
+
+/**
+ * View Transaction Component.
+ * TODO: Add support for account transfers.
+ */
+@Component({
+  selector: 'mifosx-view-transaction',
+  templateUrl: './view-transaction.component.html',
+  styleUrls: ['./view-transaction.component.scss']
+})
+export class ViewTransactionComponent {
+
+  /** Transaction data. */
+  transactionData: any;
+
+  /**
+   * Retrieves the Transaction data from `resolve`.
+   * @param {ActivatedRoute} route Activated Route.
+   * @param {MatDialog} dialog Dialog reference.
+   */
+  constructor(private route: ActivatedRoute,
+              public dialog: MatDialog) {
+    this.route.data.subscribe((data: { fixedDepositsAccountTransaction: any }) => {
+      this.transactionData = data.fixedDepositsAccountTransaction;
+    });
+  }
+
+}
