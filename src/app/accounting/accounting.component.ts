c092c9c7fcd7938edba53dd0ab5e1fd3be2fd40a
Ashutosh Sharma <ashutoshsharma0061@gmail.com>
2020-07-18 19:58:18 +0530

    feat: add accounting tour.
    add accounting tour (configuration wizard).
    
    Fixes; #1134

--- src/app/accounting/accounting.component.ts
+++ src/app/accounting/accounting.component.ts
@@ -1,5 +1,10 @@
 /** Angular Imports */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+/** Custom Services */
+import { PopoverService } from '../configuration-wizard/popover/popover.service';
+import { ConfigurationWizardService } from '../configuration-wizard/configuration-wizard.service';
 
 /**
  * Accounting component.
@@ -9,11 +14,167 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './accounting.component.html',
   styleUrls: ['./accounting.component.scss']
 })
-export class AccountingComponent implements OnInit {
+export class AccountingComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  /* Reference of Chart of Accounts */
+  @ViewChild('chartofAccounts') chartofAccounts: ElementRef<any>;
+  /* Template for popover on Chart of Accounts */
+  @ViewChild('templateChartofAccounts') templateChartofAccounts: TemplateRef<any>;
+  /* Reference of Accounts Linked */
+  @ViewChild('accountsLinked') accountsLinked: ElementRef<any>;
+  /* Template for popover on Accounts  Linked */
+  @ViewChild('templateAccountsLinked') templateAccountsLinked: TemplateRef<any>;
+  /* Reference of Migrate Opening Balances */
+  @ViewChild('migrateOpeningBalances') migrateOpeningBalances: ElementRef<any>;
+  /* Template for popover on Migrate Opening Balances */
+  @ViewChild('templateMigrateOpeningBalances') templateMigrateOpeningBalances: TemplateRef<any>;
+  /* Reference of Closing Entries */
+  @ViewChild('closingEntries') closingEntries: ElementRef<any>;
+  /* Template for popover on Closing Entries */
+  @ViewChild('templateClosingEntries') templateClosingEntries: TemplateRef<any>;
+  /* Reference of Create Journal Entries */
+  @ViewChild('createJournalEntries') createJournalEntries: ElementRef<any>;
+  /* Template for popover on Create Journal Entries */
+  @ViewChild('templateCreateJournalEntries') templateCreateJournalEntries: TemplateRef<any>;
+
+  /**
+   * @param {Router} router Router.
+   * @param {ConfigurationWizardService} configurationWizardService ConfigurationWizard Service.
+   * @param {PopoverService} popoverService PopoverService.
+   */
+  constructor(private router: Router,
+              private configurationWizardService: ConfigurationWizardService,
+              private popoverService: PopoverService) { }
 
   ngOnInit() {
+  }
+
+  /**
+   * Popover function
+   * @param template TemplateRef<any>.
+   * @param target HTMLElement | ElementRef<any>.
+   * @param position String.
+   * @param backdrop Boolean.
+   */
+  showPopover(template: TemplateRef<any>, target: HTMLElement | ElementRef<any>, position: string, backdrop: boolean): void {
+    setTimeout(() => this.popoverService.open(template, target, position, backdrop, {}), 200);
+  }
+
+  /**
+   * To show popover.
+   */
+  ngAfterViewInit() {
+    if (this.configurationWizardService.showChartofAccounts === true) {
+      setTimeout(() => {
+        this.showPopover(this.templateChartofAccounts, this.chartofAccounts.nativeElement, 'bottom', true);
+      });
+    }
+    if (this.configurationWizardService.showAccountsLinked === true) {
+      setTimeout(() => {
+        this.showPopover(this.templateAccountsLinked, this.accountsLinked.nativeElement, 'bottom', true);
+      });
+    }
+    if (this.configurationWizardService.showMigrateOpeningBalances === true) {
+      setTimeout(() => {
+        this.showPopover(this.templateMigrateOpeningBalances, this.migrateOpeningBalances.nativeElement, 'bottom', true);
+      });
+    }
+    if (this.configurationWizardService.showClosingEntries === true) {
+      setTimeout(() => {
+        this.showPopover(this.templateClosingEntries, this.closingEntries.nativeElement, 'bottom', true);
+      });
+    }
+    if (this.configurationWizardService.showCreateJournalEntries === true) {
+      setTimeout(() => {
+        this.showPopover(this.templateCreateJournalEntries, this.createJournalEntries.nativeElement, 'bottom', true);
+      });
+    }
+  }
+
+  /**
+   * Next Step (Charts of Accounts Page) Configuration Wizard.
+   */
+  nextStepChartofAccounts() {
+    this.configurationWizardService.showChartofAccounts = false;
+    this.configurationWizardService.showChartofAccountsPage = true;
+    this.router.navigate(['/accounting/chart-of-accounts']);
+  }
+
+  /**
+   * Previous Step (Scheduler Jobs Page) Configuration Wizard.
+   */
+  previousStepChartofAccounts() {
+    this.configurationWizardService.showChartofAccounts = false;
+    this.configurationWizardService.showSchedulerJobsList = true;
+    this.router.navigate(['/system/scheduler-jobs']);
+  }
+
+  /**
+   * Next Step (Accounts Linked Page) Configuration Wizard.
+   */
+  nextStepAccountsLinked() {
+    this.configurationWizardService.showAccountsLinked = false;
+    this.configurationWizardService.showAccountsLinkedPage = true;
+    this.router.navigate(['/accounting/financial-activity-mappings']);
+  }
+
+  /**
+   * Previous Step (Create Chart of Accounts Page) Configuration Wizard.
+   */
+  previousStepAccountsLinked() {
+    this.configurationWizardService.showAccountsLinked = false;
+    this.configurationWizardService.showChartofAccountsForm = true;
+    this.router.navigate(['/accounting/chart-of-accounts/gl-accounts/create']);
+  }
+
+  /**
+   * Next Step (Migrate Opening Balances) Configuration Wizard.
+   */
+  nextStepMigrateOpeningBalances() {
+    this.router.navigate(['/accounting/migrate-opening-balances']);
+  }
+
+  /**
+   * Previous Step (Accounts Linked Page) Configuration Wizard.
+   */
+  previousStepMigrateOpeningBalances() {
+    this.configurationWizardService.showMigrateOpeningBalances = false;
+    this.configurationWizardService.showAccountsLinkedList = true;
+    this.router.navigate(['accounting/financial-activity-mappings']);
+  }
+
+  /**
+   * Next Step (Closing Entries Page) Configuration Wizard.
+   */
+  nextStepClosingEntries() {
+    this.configurationWizardService.showClosingEntries = false;
+    this.configurationWizardService.showClosingEntriesPage = true;
+    this.router.navigate(['/accounting/closing-entries']);
+  }
+
+  /**
+   * Previous Step (Migrate Opening Balances Page) Configuration Wizard.
+   */
+  previousStepClosingEntries() {
+    this.configurationWizardService.showClosingEntries = false;
+    this.configurationWizardService.showMigrateOpeningBalances = true;
+    this.router.navigate(['/accounting/migrate-opening-balances']);
+  }
+
+  /**
+   * Next Step (Create Journal Entries Page) Configuration Wizard.
+   */
+  nextStepCreateJournalEntries() {
+    this.router.navigate(['/accounting/journal-entries/create']);
+  }
+
+  /**
+   * Previous Step (Closing Entries Page) Configuration Wizard.
+   */
+  previousStepCreateJournalEntries() {
+    this.configurationWizardService.showCreateJournalEntries = false;
+    this.configurationWizardService.showClosingEntriesList = true;
+    this.router.navigate(['/accounting/closing-entries']);
   }
 
 }
--- src/app/accounting/closing-entries/closing-entries.component.ts
+++ src/app/accounting/closing-entries/closing-entries.component.ts
@@ -1,11 +1,15 @@
 /** Angular Imports */
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { FormControl } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 /** rxjs Imports */
 import { startWith, map } from 'rxjs/operators';
+
+/** Custom Services */
+import { PopoverService } from '../../configuration-wizard/popover/popover.service';
+import { ConfigurationWizardService } from '../../configuration-wizard/configuration-wizard.service';
 
 /**
  * Closing entries component.
@@ -15,7 +19,7 @@ import { startWith, map } from 'rxjs/operators';
   templateUrl: './closing-entries.component.html',
   styleUrls: ['./closing-entries.component.scss']
 })
-export class ClosingEntriesComponent implements OnInit {
+export class ClosingEntriesComponent implements OnInit, AfterViewInit {
 
   /** Columns to be displayed in closing entries table. */
   displayedColumns: string[] = ['officeName', 'closingDate', 'comments', 'createdByUsername'];
@@ -35,11 +39,26 @@ export class ClosingEntriesComponent implements OnInit {
   /** Sorter for closing entries table. */
   @ViewChild(MatSort) sort: MatSort;
 
+  /* Reference of create closure button */
+  @ViewChild('buttonCreateClosure') buttonCreateClosure: ElementRef<any>;
+  /* Template for popover on create closure button */
+  @ViewChild('templateButtonCreateClosure') templateButtonCreateClosure: TemplateRef<any>;
+  /* Reference of closures table */
+  @ViewChild('closuresTable') closuresTable: ElementRef<any>;
+  /* Template for popover on closures table */
+  @ViewChild('templateClosuresTable') templateClosuresTable: TemplateRef<any>;
+
   /**
    * Retrieves the offices and gl account closures data from `resolve`.
    * @param {ActivatedRoute} route Activated Route.
+   * @param {Router} router Router.
+   * @param {ConfigurationWizardService} configurationWizardService ConfigurationWizard Service.
+   * @param {PopoverService} popoverService PopoverService.
    */
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private configurationWizardService: ConfigurationWizardService,
+              private popoverService: PopoverService) {
     this.route.data.subscribe((data: {
       offices: any,
       glAccountClosures: any
@@ -97,4 +116,51 @@ export class ClosingEntriesComponent implements OnInit {
     return this.officeData.filter((office: any) => office.name.toLowerCase().includes(officeName.toLowerCase()));
   }
 
+  /**
+   * Popover function
+   * @param template TemplateRef<any>.
+   * @param target HTMLElement | ElementRef<any>.
+   * @param position String.
+   * @param backdrop Boolean.
+   */
+  showPopover(template: TemplateRef<any>, target: HTMLElement | ElementRef<any>, position: string, backdrop: boolean): void {
+    setTimeout(() => this.popoverService.open(template, target, position, backdrop, {}), 200);
+  }
+
+  /**
+   * To show popover.
+   */
+  ngAfterViewInit() {
+    if (this.configurationWizardService.showClosingEntriesPage === true) {
+      setTimeout(() => {
+        this.showPopover(this.templateButtonCreateClosure, this.buttonCreateClosure.nativeElement, 'bottom', true);
+      });
+    }
+
+    if (this.configurationWizardService.showClosingEntriesList === true) {
+      setTimeout(() => {
+        this.showPopover(this.templateClosuresTable, this.closuresTable.nativeElement, 'top', true);
+      });
+    }
+  }
+
+  /**
+   * Next Step (Create journal Entry Accounting Page) Configuration Wizard.
+   */
+  nextStep() {
+    this.configurationWizardService.showClosingEntriesPage = false;
+    this.configurationWizardService.showClosingEntriesList = false;
+    this.configurationWizardService.showCreateJournalEntries = true;
+    this.router.navigate(['/accounting']);
+  }
+
+  /**
+   * Previous Step (Closing Entries Accounting Page) Configuration Wizard.
+   */
+  previousStep() {
+    this.configurationWizardService.showClosingEntriesPage = false;
+    this.configurationWizardService.showClosingEntriesList = false;
+    this.configurationWizardService.showClosingEntries = true;
+    this.router.navigate(['/accounting']);
+  }
 }
