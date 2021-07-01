47bf9db86ee88688874ce2d86c568ee299e031e7
Shreyank Byadagi <shreyankbyadagi@Shreyanks-MacBook-Pro.local>
2021-07-01 18:48:19 +0530

    shreyank: Dockerize mifos web-app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ import env from './.env';
 
 export const environment = {
   production: true,
-  version: env.mifos_x_version,
+  version: env[mifos_x_version],
   fineractPlatformTenantId: 'default',  // For connecting to server running elsewhere update the tenant identifier
   baseApiUrl: JSON.parse(localStorage.getItem('mifosXServerURL')) || 'https://demo.fineract.dev',  // For connecting to server running elsewhere update the base API URL
-  allowServerSwitch: env.allow_switching_backend_instance,
+  allowServerSwitch: env[allow_switching_backend_instance],
   apiProvider: '/fineract-provider/api',
   apiVersion: '/v1',
   serverUrl: '',
