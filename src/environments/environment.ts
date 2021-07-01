058a624a34b17198dc440e467bc23bfdb8e66c5c
Shreyank Byadagi <57580641+shrunk3@users.noreply.github.com>
2021-07-01 23:06:06 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,10 +8,10 @@ import env from './.env';
 
 export const environment = {
   production: false,
-  version: '1.0',
+  version: env.mifos_x_version,
   fineractPlatformTenantId: 'default',  // For connecting to server running elsewhere update the tenant identifier
   baseApiUrl: JSON.parse(localStorage.getItem('mifosXServerURL')) || 'https://dev.mifos.io',  // For connecting to server running elsewhere update the base API URL
-  allowServerSwitch: 'true',
+  allowServerSwitch: env.allow_switching_backend_instance,
   apiProvider: '/fineract-provider/api',
   apiVersion: '/v1',
   serverUrl: '',
