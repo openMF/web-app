7469fe5551615ac41dd5be9f62d08cc7437f1086
xurror <kaze.nasser@outlook.com>
2021-07-30 16:05:44 +0100

    Add safe checks to charges datasource in loan product charges step

--- src/app/products/loan-products/loan-product-stepper/loan-product-charges-step/loan-product-charges-step.component.ts
+++ src/app/products/loan-products/loan-product-stepper/loan-product-charges-step/loan-product-charges-step.component.ts
@@ -28,7 +28,7 @@ export class LoanProductChargesStepComponent implements OnInit {
 
   ngOnInit() {
     this.chargeData = this.loanProductsTemplate.chargeOptions;
-    this.overdueChargeData = this.loanProductsTemplate.penaltyOptions.filter((penalty: any) => penalty.chargeTimeType.code === 'chargeTimeType.overdueInstallment');
+    this.overdueChargeData = this.loanProductsTemplate?.penaltyOptions?.filter((penalty: any) => penalty.chargeTimeType.code === 'chargeTimeType.overdueInstallment');
 
     this.chargesDataSource = this.loanProductsTemplate.charges || [];
     this.pristine = true;
