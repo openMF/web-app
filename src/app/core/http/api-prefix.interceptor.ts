468aebceb7744c18ceb2efe3d2a3aa408957cec9
Nestor Lazcano <nestor_ronal09@hotmail.com>
2021-08-05 11:04:06 -0500

    remove line with comments in app.module, remove logic in api-prefix and add empty line at the EOF

--- src/app/core/http/api-prefix.interceptor.ts
+++ src/app/core/http/api-prefix.interceptor.ts
@@ -18,14 +18,7 @@ export class ApiPrefixInterceptor implements HttpInterceptor {
    * Intercepts a Http request and prefixes it with `environment.serverUrl`.
    */
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    /**
-     * Ignore URLs that are complete for i18n
-     */
-    if (!request.url.includes('http:') && !request.url.includes('https:')) {
-      request = request.clone({ url: environment.serverUrl + request.url });
-    }
-    
+    request = request.clone({ url: environment.serverUrl + request.url });
     return next.handle(request);
   }
 
