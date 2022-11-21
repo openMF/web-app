7df9e827a1bea8ee6159c0f785036b8879c6f05d
mikebarasa <michael.barasa@oneacrefund.org>
2022-11-21 11:33:23 +0300

    Add cypress test framewok

---
+++ tests/frontend/cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 60000,
+  pageLoadTimeout: 60000,
+  viewportHeight: 900,
+  viewportWidth: 1400,
+  watchForFileChanges: false,
+  chromeWebSecurity: false,
+  trashAssetsBeforeRuns: true,
+  video: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }
+  },
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/results-[hash].xml'
+  },
+})
