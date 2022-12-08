a18386daafcc4825cc22029c4893532425b22c55
mikebarasa <michael.barasa@oneacrefund.org>
2022-12-08 09:01:15 +0300

    Upgrade to cypress v12

--- tests/frontend/cypress.config.js
+++ tests/frontend/cypress.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
   trashAssetsBeforeRuns: true,
   video: false,
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
       // implement node event listeners here
     }
   },
