735de680fe061ede05e28ca6f4f163be8bf4437f
Jose Alberto Hernandez <alberto@MacBook-Pro.local>
2023-02-14 10:14:50 -0600

    New GL mapping parameters at the loan product level for Charge-off

--- src/app/products/charges/edit-charge/edit-charge.component.ts
+++ src/app/products/charges/edit-charge/edit-charge.component.ts
@@ -125,7 +125,7 @@ export class EditChargeComponent implements OnInit {
     if (this.chargeData.taxGroup) {
       this.chargeForm.addControl('taxGroupId', this.formBuilder.control({ value: this.chargeData.taxGroup.id, disabled: true }, Validators.required));
     } else {
-      this.chargeForm.addControl('taxGroupId', this.formBuilder.control({ value: '?', disabled: true }));
+      this.chargeForm.addControl('taxGroupId', this.formBuilder.control({ value: '', disabled: true }));
     }
   }
 
@@ -147,7 +147,7 @@ export class EditChargeComponent implements OnInit {
    * Submits Edit Charge form.
    */
   submit() {
-    const charges = this.chargeForm.value;
+    const charges = this.chargeForm.getRawValue();
     charges.locale = this.settingsService.language.code;
     charges.chargePaymentMode = this.chargeData.chargePaymentMode.id;
     this.productsService.updateCharge(this.chargeData.id.toString(), charges)
