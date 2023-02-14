a23bac81fb3c63d4409d1b6c75b0b52a6e531a97
Pushpendra Kumar <b20122@students.iitmandi.ac.in>
2023-02-14 20:34:14 +0530

    feat:add tooltip in floating rate screen
    added tooltips in floating rate screen to provide valuable aid in understanding the functionality of the system.
    
    Fixes: #1673

--- src/app/products/floating-rates/edit-floating-rate/edit-floating-rate.component.ts
+++ src/app/products/floating-rates/edit-floating-rate/edit-floating-rate.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { TooltipPosition } from '@angular/material/tooltip';
 
 /** Custom Services */
 import { ProductsService } from '../../products.service';
