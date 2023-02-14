b186399555dc6a52773dbb63234a4a07f626833d
Pushpendra Kumar <b20122@students.iitmandi.ac.in>
2023-02-14 14:47:50 +0530

    fix: family details date of birth issue
    now we can select the dob of a family member before 2000 year also ****
    
    Fixes: #1682

--- src/app/clients/clients-view/family-members-tab/edit-family-member/edit-family-member.component.ts
+++ src/app/clients/clients-view/family-members-tab/edit-family-member/edit-family-member.component.ts
@@ -18,8 +18,7 @@ import { Dates } from 'app/core/utils/dates';
 })
 export class EditFamilyMemberComponent implements OnInit {
 
-  /** Minimum Due Date allowed. */
-  minDate = new Date(2000, 0, 1);
+
   /** Maximum Due Date allowed. */
   maxDate = new Date();
   /** Add family member form. */
