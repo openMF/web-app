549de26ecc2dbea9e3ee4d7a7eb1180fc38e7351
Sabin Sheja <62881592+shejasabin@users.noreply.github.com>
2023-06-15 14:34:50 +0200

    chore: target qa backend

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,8 +2,8 @@
   window["env"] = window["env"] || {};
 
   // BackEnd Environment variables
-  window["env"]["fineractApiUrls"] = 'https://accounts.test.oneacrefund.org';
-  window["env"]["fineractApiUrl"]  = 'https://accounts.test.oneacrefund.org';
+  window["env"]["fineractApiUrls"] = 'https://accounts.qa.oneacrefund.org';
+  window["env"]["fineractApiUrl"]  = 'https://accounts.qa.oneacrefund.org';
 
   window["env"]["apiProvider"] = '/fineract-provider/api';
   window["env"]["apiVersion"]  = '/v1';
