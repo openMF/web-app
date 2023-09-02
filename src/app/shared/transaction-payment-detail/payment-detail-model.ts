2bdcb69366a9f94e081ac5cdf219c55de09ae0b8
José Alberto Hernández <44206706+josehernandezfintecheandomx@users.noreply.github.com>
2023-09-02 16:28:01 -0600

    Savings account transaction with datatables (#1858)
    Co-authored-by: Jose Alberto Hernandez <alberto@blue-daemon.local>

---
+++ src/app/shared/transaction-payment-detail/payment-detail-model.ts
@@ -0,0 +1,16 @@
+export interface PaymentType {
+  id: number;
+  isSystemDefined: boolean;
+  name: string;
+}
+
+export interface PaymentDetail {
+  paymentType: PaymentType;
+  accountNumber: string;
+  checkNumber: string;
+  routingCode: string;
+  receiptNumber: string;
+  bankNumber: string;
+}
+
+
