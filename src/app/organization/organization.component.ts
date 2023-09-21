8b47f4f3df05f1551563d629f70171ff55c9c6d7
Onyx2406 <63511758+onyx243@users.noreply.github.com>
2023-09-21 14:34:14 +0530

    Fixed condition issue

--- src/app/organization/organization.component.ts
+++ src/app/organization/organization.component.ts
@@ -16,7 +16,7 @@ import { ConfigurationWizardService } from '../configuration-wizard/configuratio
 })
 export class OrganizationComponent implements OnInit, AfterViewInit {
 
-  shouldShowFundMapping: boolean = true;
+  shouldShowFundMapping: boolean = false;
   /* Reference of manage offices */
   @ViewChild('office') office: ElementRef<any>;
   /* Template for popover on manage offices */
--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '0.0.0',
-    'hash': '9b40d307'
+    'version': '230920',
+    'hash': '4062c669'
   },
   'allow_switching_backend_instance': true
 };
