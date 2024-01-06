9668c2072668d2d2a795ecea42e8c15e8fa48aaf
HitanshuPrasad <hitanshuprasad@gmail.com>
2024-01-07 00:34:31 +0530

    Top PAdding for Kyeboard Shortcuts Dialog

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '231121',
-    'hash': 'f6edab0d'
+    'version': '240101',
+    'hash': '88ef2996'
   },
   'allow_switching_backend_instance': true
 };
