cfa7d60c0adbb6bd9173e3a4463baa27877c59e4
Jose Alberto Hernandez <alberto@blue-daemon.local>
2024-01-11 21:22:39 -0600

    Add checkbox to hide accrual transactions on deposits

--- src/app/deposits/recurring-deposits/recurring-deposits-account-view/transactions-tab/transactions-tab.component.ts
+++ src/app/deposits/recurring-deposits/recurring-deposits-account-view/transactions-tab/transactions-tab.component.ts
@@ -1,5 +1,6 @@
 /** Angular Imports */
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormControl } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -17,6 +18,10 @@ export class TransactionsTabComponent implements OnInit {
   status: any;
   /** Transactions Data */
   transactionsData: any;
+  /** Temporary Transaction Data */
+  tempTransaction: any;
+  /** Form control to handle accural parameter */
+  hideAccrualsParam: UntypedFormControl;
   /** Columns to be displayed in transactions table. */
   displayedColumns: string[] = ['id', 'transactionDate', 'transactionType', 'debit', 'credit', 'balance'];
   /** Data source for transactions table. */
@@ -30,12 +35,23 @@ export class TransactionsTabComponent implements OnInit {
               private router: Router) {
     this.route.parent.data.subscribe((data: { recurringDepositsAccountData: any }) => {
       this.transactionsData = data.recurringDepositsAccountData.transactions;
+      this.tempTransaction = this.transactionsData;
       this.status = data.recurringDepositsAccountData.status.value;
     });
   }
 
   ngOnInit() {
     this.dataSource = new MatTableDataSource(this.transactionsData);
+    this.hideAccrualsParam = new UntypedFormControl(false);
+    this.tempTransaction.forEach((element: any) => {
+      if (this.isAccrual(element.transactionType)) {
+        this.tempTransaction = this.removeItem(this.tempTransaction, element);
+      }
+    });
+  }
+
+  private removeItem(arr: any, item: any) {
+    return arr.filter((f: any) => f !== item);
   }
 
   /**
@@ -45,6 +61,29 @@ export class TransactionsTabComponent implements OnInit {
   isDebit(transactionType: any) {
     return transactionType.withdrawal === true || transactionType.feeDeduction === true
       || transactionType.overdraftInterest === true || transactionType.withholdTax === true;
+  }
+
+  isAccrual(transactionType: any): boolean {
+    return (transactionType.accrual || transactionType.code === 'savingsAccountTransactionType.accrual');
+  }
+
+  hideAccruals() {
+    if (!this.hideAccrualsParam.value) {
+      this.dataSource = new MatTableDataSource(this.tempTransaction);
+    } else {
+      this.dataSource = new MatTableDataSource(this.transactionsData);
+    }
+  }
+
+  /**
+   * Checks transaction status.
+   */
+  checkStatus() {
+    if (this.status === 'Active' || this.status === 'Closed' || this.status === 'Transfer in progress' ||
+       this.status === 'Transfer on hold' || this.status === 'Premature Closed' || this.status === 'Matured') {
+      return true;
+    }
+    return false;
   }
 
   /**
