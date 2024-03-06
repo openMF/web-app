58c69dc1652c6c0a67e4096a89efcb97a0793633
amy.muhimpundu <amy.muhimpundu@oneacrefund.org>
2024-03-06 18:36:33 +0200

    feat/SER-2045 Add delete button for loan product

--- src/app/products/loan-products/view-loan-product/view-loan-product.component.ts
+++ src/app/products/loan-products/view-loan-product/view-loan-product.component.ts
@@ -1,29 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { OrganizationService } from 'app/organization/organization.service';
-import { OUHierarchy } from 'app/hierarchy-generator';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { OrganizationService } from "app/organization/organization.service";
+import { OUHierarchy } from "app/hierarchy-generator";
+import { MatDialog } from "@angular/material/dialog";
+import { DeleteDialogComponent } from "app/shared/delete-dialog/delete-dialog.component";
+import { ProductsService } from "app/products/products.service";
 
 @Component({
-  selector: 'mifosx-view-loan-product',
-  templateUrl: './view-loan-product.component.html',
-  styleUrls: ['./view-loan-product.component.scss'],
+  selector: "mifosx-view-loan-product",
+  templateUrl: "./view-loan-product.component.html",
+  styleUrls: ["./view-loan-product.component.scss"],
 })
 export class ViewLoanProductComponent implements OnInit {
   loanProduct: any;
   loanProductOffices: any;
   arrayList: any = [];
   variationsDisplayedColumns: string[] = [
-    'valueConditionType',
-    'borrowerCycleNumber',
-    'minValue',
-    'defaultValue',
-    'maxValue',
+    "valueConditionType",
+    "borrowerCycleNumber",
+    "minValue",
+    "defaultValue",
+    "maxValue",
   ];
-  chargesDisplayedColumns: string[] = ['name', 'chargeCalculationType', 'amount', 'chargeTimeType'];
-  paymentFundSourceDisplayedColumns: string[] = ['paymentTypeId', 'fundSourceAccountId'];
-  feesPenaltyIncomeDisplayedColumns: string[] = ['chargeId', 'incomeAccountId'];
+  chargesDisplayedColumns: string[] = ["name", "chargeCalculationType", "amount", "chargeTimeType"];
+  paymentFundSourceDisplayedColumns: string[] = ["paymentTypeId", "fundSourceAccountId"];
+  feesPenaltyIncomeDisplayedColumns: string[] = ["chargeId", "incomeAccountId"];
 
-  constructor(private route: ActivatedRoute, private organizationService: OrganizationService, public ouHierarchy: OUHierarchy ) {
+  constructor(
+    private productService: ProductsService,
+    private organizationService: OrganizationService,
+    private route: ActivatedRoute,
+    private router: Router,
+    public ouHierarchy: OUHierarchy,
+    private dialog: MatDialog
+  ) {
     this.route.data.subscribe((data: { loanProduct: any }) => {
       this.loanProduct = data.loanProduct;
     });
@@ -34,7 +44,7 @@ export class ViewLoanProductComponent implements OnInit {
       (attribute: boolean) => attribute
     );
 
-    if ( this.loanProduct?.offices?.length > 0) {
+    if (this.loanProduct?.offices?.length > 0) {
       this.organizationService.searchCountryById(this.loanProduct.countryId).subscribe((res: any) => {
         const data = res.map((item: any) => ({
           name: item.name,
@@ -56,23 +66,40 @@ export class ViewLoanProductComponent implements OnInit {
           parentId: item.parentId,
         }));
 
-        const response = this.ouHierarchy.flatToHierarchy(newTreeView, data.filter(x => x.id !== this.loanProduct.countryId));
+        const response = this.ouHierarchy.flatToHierarchy(
+          newTreeView,
+          data.filter((x) => x.id !== this.loanProduct.countryId)
+        );
         if (response?.length > 0) {
           this.arrayList = response.filter((x) => x.children?.length > 0);
           this.arrayList.forEach((resp) => {
             const officeCount = data.filter((x) => x.parentId === resp.id);
             if (resp?.children?.length === officeCount?.length) {
-              resp.children = 'All OU\'s';
+              resp.children = "All OU's";
             } else {
               resp.children = resp.children
                 .map((x) => {
                   return x.name;
                 })
-                .join(', ');
+                .join(", ");
             }
           });
         }
       });
     }
   }
+
+  deleteLoanProduct = () => {
+    const deleteSLoanProductDialogRef = this.dialog.open(DeleteDialogComponent, {
+      data: { deleteContext: `loan product with id: ${this.loanProduct.id}` },
+    });
+    deleteSLoanProductDialogRef.afterClosed().subscribe((response: any) => {
+      if (response.delete) {
+        console.log("Confirmed deletion:");
+        this.productService.deleteLoanProduct(this.loanProduct.id).subscribe(() => {
+          this.router.navigate(["/products/loan-products"]);
+        });
+      }
+    });
+  };
 }
