d9eb37a577d72d5ed1d8efe4f48993e6f765f564
Vikash Prem Sharma <vikashkumarukg977@gmail.com>
2024-03-08 00:50:44 +0530

    feat: Implement Dark Mode
    Added styles and configurations to enable dark mode throughout the application. The dark mode features a deep blue color scheme for primary elements and improved visibility for cursor. Selected option text in mat-select now appears in white for better contrast. This enhancement provides users with the option to switch between light and dark modes seamlessly.
    
    Changes:
    - Added dark mode styles in the theme service
    - Updated mifosx-theme.scss for dark mode customization
    - Adjusted cursor visibility in dark mode
    - Modified selected option text color in mat-select
    
    Fixes #2021
    
    Implemented the black-logo image into white-logo-image dynamically

---
+++ src/app/theme.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private darkMode = false;
+
+  isDarkMode() {
+    return this.darkMode;
+  }
+
+  setDarkMode(isDarkMode: boolean) {
+    this.darkMode = isDarkMode;
+    if (isDarkMode) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
+}
