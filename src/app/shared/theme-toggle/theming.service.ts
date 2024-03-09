f8bbd270f5e84e3b2f521e3db509a6aed016802c
Jose Alberto Hernandez <alberto@black-box.local>
2024-03-09 00:51:29 -0600

    Login view with the support of dark theme

--- src/app/shared/theme-toggle/theming.service.ts
+++ src/app/shared/theme-toggle/theming.service.ts
@@ -7,24 +7,22 @@ import { BehaviorSubject } from 'rxjs';
 export class ThemingService {
   private darkModeOn = false;
 
-  themes = ["dark-theme", "light-theme"]; // <- list all themes in this array
-  theme = new BehaviorSubject("light-theme"); // <- initial theme
+  themes = ['dark-theme', 'light-theme']; // <- list all themes in this array
+  theme = new BehaviorSubject('light-theme'); // <- initial theme
 
   constructor(private ref: ApplicationRef) {
     // Initially check if dark mode is enabled on system
     this.darkModeOn =
       window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
-    
+      window.matchMedia('(prefers-color-scheme: dark)').matches;
+
     // If dark mode is enabled then directly switch to the dark-theme
-    if(this.darkModeOn){
-      this.theme.next("dark-theme");
-    }
+    this.setDarkMode(this.darkModeOn);
 
     // Watch for changes of the preference
-    window.matchMedia("(prefers-color-scheme: dark)").addListener(e => {
+    window.matchMedia('(prefers-color-scheme: dark)').addListener(e => {
       const turnOn = e.matches;
-      this.theme.next(turnOn ? "dark-theme" : "light-theme");
+      this.theme.next(turnOn ? 'dark-theme' : 'light-theme');
 
       // Trigger refresh of UI
       this.ref.tick();
@@ -34,7 +32,7 @@ export class ThemingService {
   isDarkMode(): boolean {
     this.darkModeOn =
       window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
+      window.matchMedia('(prefers-color-scheme: dark)').matches;
     return this.darkModeOn;
   }
 
@@ -42,10 +40,14 @@ export class ThemingService {
     this.darkModeOn = isDarkMode;
     if (isDarkMode) {
       document.body.classList.add('dark-theme');
-      this.theme.next("dark-theme");
+      this.theme.next('dark-theme');
     } else {
       document.body.classList.remove('dark-theme');
-      this.theme.next("light-theme");
+      this.theme.next('light-theme');
     }
+  }
+
+  setInitialDarkMode(): void {
+    this.setDarkMode(this.darkModeOn);
   }
 }
