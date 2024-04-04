b242936e70a315b1da4c193f708daa78542a60e6
Jose Alberto Hernandez <alberto@black-box.local>
2024-04-03 20:11:48 -0600

    Client views updated

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '231121',
-    'hash': 'f6edab0d'
+    'version': '240403',
+    'hash': '54dc91e0'
   },
   'allow_switching_backend_instance': true
 };
