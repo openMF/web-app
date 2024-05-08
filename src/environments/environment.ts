2b033bb9b09c504d65a7038c298dd81cb9763b85
Magezi Arthur <artmagezi@gmail.com>
2024-05-08 11:28:24 +0300

    Fix: Environment variables and a new Variable-ALLOW_SWITCHING_BACKEND_INSTANCE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,7 +18,7 @@ export const environment = {
     'https://dev.mifos.io,https://demo.mifos.io,https://qa.mifos.io,https://staging.mifos.io,https://mobile.mifos.io,https://demo.fineract.dev,https://localhost:8443,http://localhost:8443',
   // For connecting to server running elsewhere set the base API URL
   baseApiUrl: window['env']['baseApiUrl'] || 'https://demo.fineract.dev',
-  allowServerSwitch: env.allow_switching_backend_instance,
+  allowServerSwitch: window['env']['allowSwitchingBackendInstance'] || true,
   apiProvider: window['env']['apiProvider'] || '/fineract-provider/api',
   apiVersion: window['env']['apiVersion'] || '/v1',
   serverUrl: '',
