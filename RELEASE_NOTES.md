06a8121b356d45fc138220beaa8eb5535439c492
Gilbert Adjin Frimpong <gilbert.frimpong@oneacrefund.org>
2024-05-21 06:37:32 +0000

    Ser1561/office view fixes (#107)
    * feat: Hide the add button on the lowest level OU in the tree
    
    * refactor: Remove unused import in offices.component.ts

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -44,6 +44,8 @@ Release Notes
     * [SER-1912] - Fineract v2  - Show OU Location Hierarchy Details When Displaying Clients
     * [SER-1911] - Fineract v2  - Show OU Location Hierarchy Details When Displaying Groups
     * [SER-2486] - Creating savings account and mapping it to loans and country
+    * [SER-1896] - Hide the add button on the lowest level OU in the tree.
+    * [SER-1561] - show the proper office name instead of the name decorated.
 
 ## Version 1.0.0 - for use with Fineract Web App
 
--- src/app/shared/office-tree-view/office-tree-node.ts
+++ src/app/shared/office-tree-view/office-tree-node.ts
@@ -1,37 +1,37 @@
 export class OfficeTreeNode {
-    name: string;
-    id: number;
-    levelName?: string;
-    children?: OfficeTreeNode[];
-    checked: boolean;
-    constructor() {
-
-    }
+  name: string;
+  id: number;
+  levelName?: string;
+  children?: OfficeTreeNode[];
+  checked: boolean;
+  isLowestOU?: boolean;
 }
 
 export class OfficeFlatNode {
-    expandable: boolean;
-    name: string;
-    level: number;
-    id: number;
-    hasChild: boolean;
-    checked: boolean;
-  }
+  expandable: boolean;
+  name: string;
+  level: number;
+  id: number;
+  hasChild?: boolean;
+  checked?: boolean;
+  isLowestOU?: boolean;
+}
 
-  export class OfficeHierarchy {
-    levelName: string;
-    hierarchyType = 'OAF';
-    descendant?: OfficeHierarchy[];
-    children: any = [];
-    collapsed = false;
-    root = true;
-    selected = 'selected';
-    parentId: any = null;
-  }
-  export class OfficeHierarchyFlatNode {
-    expandable: boolean;
-    levelName: string;
-    level: number;
-    id: number;
-    hasChild: boolean;
-  }
+export class OfficeHierarchy {
+  levelName: string;
+  hierarchyType = 'OAF';
+  descendant?: OfficeHierarchy[];
+  children: any = [];
+  collapsed = false;
+  root = true;
+  selected = 'selected';
+  parentId: any = null;
+  isLowestOU?: boolean;
+}
+export class OfficeHierarchyFlatNode {
+  expandable: boolean;
+  levelName: string;
+  level: number;
+  id: number;
+  hasChild: boolean;
+}
