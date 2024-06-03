7181ccc471db1a171d37f47dc3f76edacb6f7e3f
Omar-Nabil <omarabosalem66@gmail.com>
2024-06-03 20:40:24 +0300

    create new translate pipe and remove TranslateCatalogPipe

---
+++ src/app/pipes/translate.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { TranslatePipe } from './translate.pipe';
+
+describe('Pipe: Translatee', () => {
+  it('create an instance', () => {
+    let pipe = new TranslatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/translate.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Pipe({
+  name: 'translateKey'
+})
+export class TranslatePipe implements PipeTransform {
+
+  constructor(private translateService: TranslateService) { }
+
+  transform(attributeValue: any, group: string, prefix: string = 'labels'): string {
+    const translationKey = `${prefix}.${group}.${attributeValue}`;
+    return this.translateService.instant(translationKey);
+  }
+
+}
