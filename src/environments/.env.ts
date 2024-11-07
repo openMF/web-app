032d573af5973e3627c149b3e9bb2082e940624f
Odongfelix <91344900+Odongfelix@users.noreply.github.com>
2024-11-07 17:46:48 +0300

    connecting to the backend

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '241105',
-    'hash': 'a17fc519'
+    'version': '241107',
+    'hash': '9ff6b90e'
   },
   'allow_switching_backend_instance': true
 };
