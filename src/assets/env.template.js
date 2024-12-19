FintecheandoMX/web-app-oauth

Add Oauth Server as variables

src/assets/env.template.js
@@ -35,4 +35,10 @@
   // Time in milliseconds for Session idle timeout, default 300000 seconds
   window['env']['sessionIdleTimeout'] = '$MIFOS_SESSION_IDLE_TIMEOUT';
 
+  // OAuth Server Enabled  
+  window['env']['oauthServerEnabled'] = '$MIFOS_OAUTH_SERVER_ENABLED';
+
+  // OAuth Server URL  
+  window['env']['oauthServerUrl'] = '$MIFOS_OAUTH_SERVER_URL';
+
 })(this);

openMF/web-app

src/assets/env.template.js
@@ -1,21 +1,21 @@
-(function(window) {
-  window["env"] = window["env"] || {};
+(function (window) {
+  window['env'] = window['env'] || {};
 
   // BackEnd Environment variables
-  window["env"]["fineractApiUrls"] = '$FINERACT_API_URLS';
-  window["env"]["fineractApiUrl"]  = '$FINERACT_API_URL';
+  window['env']['fineractApiUrls'] = '$FINERACT_API_URLS';
+  window['env']['fineractApiUrl'] = '$FINERACT_API_URL';
 
-  window["env"]["apiProvider"] = '$FINERACT_API_PROVIDER';
-  window["env"]["apiVersion"]  = '$FINERACT_API_VERSION';
+  window['env']['apiProvider'] = '$FINERACT_API_PROVIDER';
+  window['env']['apiVersion'] = '$FINERACT_API_VERSION';
 
-  window["env"]["fineractPlatformTenantId"]  = '$FINERACT_PLATFORM_TENANT_IDENTIFIER';
-  window["env"]["fineractPlatformTenantIds"]  = '$FINERACT_PLATFORM_TENANTS_IDENTIFIER';
+  window['env']['fineractPlatformTenantId'] = '$FINERACT_PLATFORM_TENANT_IDENTIFIER';
+  window['env']['fineractPlatformTenantIds'] = '$FINERACT_PLATFORM_TENANTS_IDENTIFIER';
 
   // Language Environment variables
-  window["env"]["defaultLanguage"] = '$MIFOS_DEFAULT_LANGUAGE';
-  window["env"]["supportedLanguages"] = '$MIFOS_SUPPORTED_LANGUAGES';
+  window['env']['defaultLanguage'] = '$MIFOS_DEFAULT_LANGUAGE';
+  window['env']['supportedLanguages'] = '$MIFOS_SUPPORTED_LANGUAGES';
 
-  window['env']['preloadClients'] = '$MIFOS_PRELOAD_CLIENTS';
+  window['env']['preloadClients'] = $MIFOS_PRELOAD_CLIENTS;
 
   // Char delimiter to Export CSV options: ',' ';' '|' ' '
   window['env']['defaultCharDelimiter'] = '$MIFOS_DEFAULT_CHAR_DELIMITER';
@@ -34,5 +34,14 @@
 
   // Time in milliseconds for Session idle timeout, default 300000 seconds
   window['env']['sessionIdleTimeout'] = '$MIFOS_SESSION_IDLE_TIMEOUT';
+
+  // OAuth Server Enabled  
+  window['env']['oauthServerEnabled'] = $MIFOS_OAUTH_SERVER_ENABLED;
+
+  // OAuth Server URL  
+  window['env']['oauthServerUrl'] = '$MIFOS_OAUTH_SERVER_URL';
+
+  // OAuth Client Id  
+  window['env']['oauthAppId'] = '$MIFOS_OAUTH_CLIENT_ID';
 
 })(this);