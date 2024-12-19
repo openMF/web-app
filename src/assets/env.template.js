ba3aef49020b9bdcce2a3ce00ffc463ee81aaa2c
Víctor Romero <46640258+IOhacker@users.noreply.github.com>
2024-12-19 16:19:07 -0600

    Fix boolean values in env.template.js (#2225)

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -15,7 +15,7 @@
   window['env']['defaultLanguage'] = '$MIFOS_DEFAULT_LANGUAGE';
   window['env']['supportedLanguages'] = '$MIFOS_SUPPORTED_LANGUAGES';
 
-  window['env']['preloadClients'] = $MIFOS_PRELOAD_CLIENTS;
+  window['env']['preloadClients'] = '$MIFOS_PRELOAD_CLIENTS';
 
   // Char delimiter to Export CSV options: ',' ';' '|' ' '
   window['env']['defaultCharDelimiter'] = '$MIFOS_DEFAULT_CHAR_DELIMITER';
@@ -36,7 +36,7 @@
   window['env']['sessionIdleTimeout'] = '$MIFOS_SESSION_IDLE_TIMEOUT';
 
   // OAuth Server Enabled  
-  window['env']['oauthServerEnabled'] = $MIFOS_OAUTH_SERVER_ENABLED;
+  window['env']['oauthServerEnabled'] = '$MIFOS_OAUTH_SERVER_ENABLED';
 
   // OAuth Server URL  
   window['env']['oauthServerUrl'] = '$MIFOS_OAUTH_SERVER_URL';
