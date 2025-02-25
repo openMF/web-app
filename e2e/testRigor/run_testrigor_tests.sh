4f356bc3166d06013c8eb2215d8056021ce92e4c
Amanda <amandassa504@gmail.com>
2025-02-25 16:34:00 -0300

    test (e2e): Add new e2e testRigor tests (#2279)
    E2e cover of the main functionalities
    CI added to build.yml
    
    Co-authored-by: Amanda Silva <amanda.silva@testrigor.com>
    Co-authored-by: Víctor Romero <46640258+IOhacker@users.noreply.github.com>

--- e2e/testRigor/run_testrigor_tests.sh
+++ e2e/testRigor/run_testrigor_tests.sh
@@ -5,15 +5,13 @@ COMMIT_NAME="$(git rev-parse --verify HEAD)"
 
 # Define default values for missing variables
 
-MIFOS_TEST_SUITE_ID="BqnXhRcaZ59ADvxHo"
-MIFOS_AUTH_TOKEN="c3903536-0371-4ba3-afeb-69cf9b408ca6"
-LOCALHOST_URL="http://localhost:4200"
+# MIFOS_TEST_SUITE_ID="$MIFOS_TEST_SUITE_ID"
+# MIFOS_AUTH_TOKEN="$MIFOS_AUTH_TOKEN"
+# LOCALHOST_URL="$MIFOS_LOCALHOST_URL"
 
 # Paths for the test cases and rules files
 TEST_CASES_PATH="e2e/testRigor/testcases/**/*.txt"
 RULES_PATH="e2e/testRigor/rules/**/*.txt"
-
-curl -I $LOCALHOST_URL:4200
 
 # Command to run the tests using the testRigor CLI
 testrigor test-suite run "$MIFOS_TEST_SUITE_ID" --token "$MIFOS_AUTH_TOKEN" --localhost --url "$LOCALHOST_URL" --test-cases-path "$TEST_CASES_PATH" --rules-path "$RULES_PATH" --branch "$BRANCH_NAME" --commit "$COMMIT_NAME"
