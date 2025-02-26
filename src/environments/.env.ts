ef82bd362d8fd54ff4423772a920b93a09bf7a01
Victor Romero <victor.romero@fintecheando.mx>
2025-02-26 14:00:55 -0600

    Apply prettier

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '241204',
-    'hash': '29cda98b'
+    'version': '250226',
+    'hash': 'ea15eecf'
   },
   'allow_switching_backend_instance': true
 };
