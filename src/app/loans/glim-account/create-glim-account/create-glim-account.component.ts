08e3dfbf6ee1f19a159a7aefa3921c3179e5943b
Felix van Hove <steinwinde@users.noreply.github.com>
2025-03-18 01:37:24 +0100

    fix: bug preventing individual loan application, improved GLIM (#2329)
    This fixes a recently introduced bug preventing individual loan
    applications. It also simplifies dramatically the GLIM process,
    improving GLIM user experience and functionality.
    
    Partially addresses: WEB-9

--- src/app/loans/glim-account/create-glim-account/create-glim-account.component.ts
+++ src/app/loans/glim-account/create-glim-account/create-glim-account.component.ts
@@ -1,15 +1,18 @@
 /** Angular Imports */
-import { Component, ViewChild, /* Felix: added: */ QueryList, ViewChildren } from '@angular/core';
+import { Component, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 /** Custom Services */
-import { LoansService } from 'app/loans/loans.service';
+import { LoansService } from '../../loans.service';
 import { SettingsService } from 'app/settings/settings.service';
+import { Dates } from 'app/core/utils/dates';
+import { ClientsService } from 'app/clients/clients.service';
 
 /** Step Components */
-import { GlimChargesStepComponent } from './glim-account-stepper/glim-charges-step/glim-charges-step.component';
-import { GlimDetailsStepComponent } from './glim-account-stepper/glim-details-step/glim-details-step.component';
-import { GlimTermsStepComponent } from './glim-account-stepper/glim-terms-step/glim-terms-step.component';
+import { LoansAccountDetailsStepComponent } from '../../loans-account-stepper/loans-account-details-step/loans-account-details-step.component';
+import { LoansAccountTermsStepComponent } from '../../loans-account-stepper/loans-account-terms-step/loans-account-terms-step.component';
+import { LoansAccountChargesStepComponent } from '../../loans-account-stepper/loans-account-charges-step/loans-account-charges-step.component';
+import { LoansAccountDatatableStepComponent } from '../../loans-account-stepper/loans-account-datatable-step/loans-account-datatable-step.component';
 
 @Component({
   selector: 'mifosx-create-glim-account',
@@ -17,33 +20,32 @@ import { GlimTermsStepComponent } from './glim-account-stepper/glim-terms-step/g
   styleUrls: ['./create-glim-account.component.scss']
 })
 export class CreateGlimAccountComponent {
-  /** Imports all the step components */
-  @ViewChild(GlimDetailsStepComponent, { static: true }) loansAccountDetailsStep: GlimDetailsStepComponent;
-  @ViewChild(GlimTermsStepComponent, { static: true }) loansAccountTermsStep: GlimTermsStepComponent;
-  @ViewChild(GlimChargesStepComponent, { static: true }) loansAccountChargesStep: GlimChargesStepComponent;
+  /** Imports all the step component */
+  @ViewChild(LoansAccountDetailsStepComponent, { static: true })
+  loansAccountDetailsStep: LoansAccountDetailsStepComponent;
+  @ViewChild(LoansAccountTermsStepComponent, { static: true }) loansAccountTermsStep: LoansAccountTermsStepComponent;
+  @ViewChild(LoansAccountChargesStepComponent, { static: true })
+  loansAccountChargesStep: LoansAccountChargesStepComponent;
+  /** Get handle on dtloan tags in the template */
+  @ViewChildren('dtloan') loanDatatables: QueryList<LoansAccountDatatableStepComponent>;
 
   /** Loans Account Template */
   loansAccountTemplate: any;
   /** Loans Account Product Template */
   loansAccountProductTemplate: any | null = null;
+  /** Table Data Source */
+  dataSource: any;
+  /** Selected Members */
+  selectedMembers: any;
+  /** Collateral Options */
+  collateralOptions: any;
   /** Multi Disburse Loan */
   multiDisburseLoan: any;
   /** Principal Amount */
   principal: any;
+  datatables: any = [];
   /** Currency Code */
   currencyCode: string;
-
-  // accountLinkingOptions: any;
-
-  // totalLoan: any;
-
-  // /** Table Data Source */
-  // dataSource: any;
-  // /** Selected Members */
-  selectedMembers: any;
-  // /** Selected Members */
-  activeClientMembers: any;
-  gsimData: any;
 
   /**
    * Sets loans account create form.
@@ -57,29 +59,53 @@ export class CreateGlimAccountComponent {
     private route: ActivatedRoute,
     private router: Router,
     private loansService: LoansService,
-    private settingsService: SettingsService
+    private settingsService: SettingsService,
+    private clientService: ClientsService,
+    private dateUtils: Dates
   ) {
-    this.route.data.subscribe((data: { loansAccountTemplate: any; gsimData: any; groupsData: any }) => {
+    this.route.data.subscribe((data: { loansAccountTemplate: any; groupsData: any }) => {
       this.loansAccountTemplate = data.loansAccountTemplate;
-      this.activeClientMembers = data.groupsData.activeClientMembers;
-      this.gsimData = data.gsimData;
+      this.dataSource = data.groupsData.activeClientMembers;
     });
   }
 
   /**
-   * Sets glim account product template and collateral template
+   * Sets loans account product template and collateral template
    * @param {any} $event API response
    */
   setTemplate($event: any) {
     this.loansAccountProductTemplate = $event;
     this.currencyCode = this.loansAccountProductTemplate.currency.code;
+    const clientId = this.loansAccountTemplate.clientId;
+    if (!!clientId) {
+      this.clientService.getCollateralTemplate(clientId).subscribe((response: any) => {
+        this.collateralOptions = response;
+      });
+    } else {
+      // Fineract API doesn't have "Group Collateral Management" endpoint; from the obsolete
+      // community app it appears getCollateralTemplate(clientId) is called as well, but it's not clear how
+      // the clientId is selected from the clientIds that belong to the group.
+      console.error('No collateral data requested from Fineract, collateral might misbehave');
+    }
     const entityId = this.loansAccountTemplate.clientId
       ? this.loansAccountTemplate.clientId
       : this.loansAccountTemplate.group.id;
+    const isGroup = this.loansAccountTemplate.clientId ? false : true;
     const productId = this.loansAccountProductTemplate.loanProductId;
-    this.loansService.getLoansAccountTemplateResource(entityId, true, productId).subscribe((response: any) => {
+    this.loansService.getLoansAccountTemplateResource(entityId, isGroup, productId).subscribe((response: any) => {
       this.multiDisburseLoan = response.multiDisburseLoan;
     });
+    this.setDatatables();
+  }
+
+  setDatatables(): void {
+    this.datatables = [];
+
+    if (this.loansAccountProductTemplate.datatables) {
+      this.loansAccountProductTemplate.datatables.forEach((datatable: any) => {
+        this.datatables.push(datatable);
+      });
+    }
   }
 
   /** Get Loans Account Details Form Data */
@@ -92,6 +118,13 @@ export class CreateGlimAccountComponent {
     return this.loansAccountTermsStep.loansAccountTermsForm;
   }
 
+  /**
+   * Retrieves savings account terms form.
+   */
+  get activeClientMembers() {
+    return this.dataSource;
+  }
+
   /** Checks whether all the forms in different steps are valid or not */
   get loansAccountFormValid() {
     return (
@@ -101,93 +134,105 @@ export class CreateGlimAccountComponent {
 
   /** Gets principal Amount */
   get loanPrincipal() {
-    return this.loansAccountChargesStep.selectedClientMembers.selectedMembers.reduce(
-      (acc: number, member: any) => acc + member.principal,
-      0
-    );
+    return this.loansAccountTermsStep.loansAccountTermsForm.value.principal;
   }
 
   /** Retrieves Data of all forms except Currency to submit the data */
   get loansAccount() {
+    this.selectedMembers = this.loansAccountChargesStep.selectedClientMembers;
     return {
       ...this.loansAccountDetailsStep.loansAccountDetails,
       ...this.loansAccountTermsStep.loansAccountTerms,
       ...this.loansAccountChargesStep.loansAccountCharges,
+      ...this.loansAccountTermsStep.loanCollateral,
       ...this.loansAccountTermsStep.disbursementData
     };
   }
 
-  // setData(client: any): any {
-  //   const locale = this.settingsService.language.code;
-  //   const dateFormat = this.settingsService.dateFormat;
-  //   // const monthDayFormat = 'dd MMMM';
-  //   const data = {
-  //     ...this.loansAccount,
-  //     charges: this.loansAccount.charges.map((charge: any) => ({
-  //       chargeId: charge.id,
-  //       amount: charge.amount
-  //     })),
-  //     clientId: client.id,
-  //     totalLoan: this.totalLoan,
-  //     loanType: 'glim',
-  //     amortizationType: 1,
-  //     isParentAccount: true,
-  //     principal: client.principal,
-  //     syncDisbursementWithMeeting: false,
-  //     expectedDisbursementDate: this.dateUtils.formatDate(this.loansAccount.expectedDisbursementDate, dateFormat),
-  //     submittedOnDate: this.dateUtils.formatDate(this.loansAccount.submittedOnDate, dateFormat),
-  //     dateFormat,
-  //     // monthDayFormat,
-  //     locale
-  //   };
-  //   data.groupId = this.loansAccountTemplate.group.id;
+  setData(client: any, totalLoan: number): any {
+    const locale = this.settingsService.language.code;
+    const dateFormat = this.settingsService.dateFormat;
+    // const monthDayFormat = 'dd MMMM';
+    const data = {
+      ...this.loansAccount,
+      charges: this.loansAccount.charges.map((charge: any) => ({
+        chargeId: charge.id,
+        amount: charge.amount
+      })),
+      clientId: client.id,
+      totalLoan: totalLoan,
+      loanType: 'glim',
+      amortizationType: 1,
+      isParentAccount: true,
+      principal: client.principal,
+      syncDisbursementWithMeeting: false,
+      expectedDisbursementDate: this.dateUtils.formatDate(this.loansAccount.expectedDisbursementDate, dateFormat),
+      submittedOnDate: this.dateUtils.formatDate(this.loansAccount.submittedOnDate, dateFormat),
+      dateFormat,
+      // monthDayFormat,
+      locale
+    };
+    data.groupId = this.loansAccountTemplate.group.id;
 
-  //   return JSON.stringify(data);
-  // }
+    delete data.principalAmount;
+    // TODO: 2025-03-17: Apparently (?!) unsupported for GLIM
+    delete data.allowPartialPeriodInterestCalculation;
+    delete data.multiDisburseLoan;
+    delete data.isFloatingInterestRate;
+
+    return JSON.stringify(data);
+  }
+
+  /** Request Body Data */
+  buildRequestData(): any[] {
+    const requestData = [];
+    const memberSelected = this.selectedMembers.selectedMembers;
+    const totalLoan = this.totalLoanAmount();
+    for (let index = 0; index < memberSelected.length; index++) {
+      requestData.push({
+        requestId: index.toString(),
+        method: 'POST',
+        relativeUrl: 'loans',
+        body: this.setData(memberSelected[index], totalLoan)
+      });
+    }
+    return requestData;
+  }
+
+  totalLoanAmount(): number {
+    let total = 0;
+    const memberSelected = this.selectedMembers.selectedMembers;
+    for (let index = 0; index < memberSelected.length; index++) {
+      total += memberSelected[index].principal;
+    }
+    return total;
+  }
 
   /**
    * Creates a new GLIM account.
    */
   submit() {
-    const locale = this.settingsService.language.code;
-    const dateFormat = this.settingsService.dateFormat;
-
-    const totalLoan = this.loanPrincipal;
-    const payloadData: any[] = [];
-    this.loansAccountChargesStep.selectedClientMembers.selectedMembers.forEach((member: any, index: number) => {
-      this.loansAccountTemplate.clientId = member.id;
-      this.loansAccountTemplate.loanPurposeId = member.loanPurposeId;
-      this.loansAccountTemplate.principal = member.principal;
-      this.loansAccountTemplate.totalLoan = totalLoan;
-
-      const payload = this.loansService.buildLoanRequestPayload(
-        this.loansAccount,
-        this.loansAccountTemplate,
-        this.loansAccountProductTemplate.calendarOptions,
-        locale,
-        dateFormat
-      );
-
-      payloadData.push(this.getWrapper(payload, index));
-    });
-
-    this.loansService.createGlimAccount(payloadData).subscribe((response: any) => {
-      this.router.navigate(
-        [
-          '../',
-          JSON.parse(response[0].body).glimId
-        ],
-        { relativeTo: this.route }
-      );
+    const data = this.buildRequestData();
+    this.loansService.createGlimAccount(data).subscribe((response: any) => {
+      const body = JSON.parse(response[0].body);
+      if (body.glimId) {
+        this.router.navigate(
+          [
+            '../',
+            body.glimId
+          ],
+          { relativeTo: this.route }
+        );
+      } else {
+        this.notify(body, data);
+      }
     });
   }
 
-  getWrapper(payload: any, requestId: number): any {
-    return {
-      body: JSON.stringify(payload),
-      method: 'POST',
-      relativeUrl: 'loans',
-      requestId: requestId
-    };
+  notify(body: any, data: any) {
+    let message = body.defaultUserMessage + ' ';
+    while (body.errors?.length > 0) message += body.errors.pop().developerMessage + ' ';
+    message += 'Data: ' + JSON.stringify(data);
+    console.error(message);
   }
 }
