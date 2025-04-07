d75220a0af596888de65f422d37ef000a33373de
urgetolearn <chinmayishaan2316@gmail.com>
2025-04-07 20:54:18 +0530

    fix: update environment files for backend connection

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '250330',
-    'hash': 'f4f480ac'
+    'version': '250407',
+    'hash': 'a2d98f90'
   },
   'allow_switching_backend_instance': true
 };
