da20da0ac415792e8e0bf913f3ee45d238edd858
steinwinde <fvanhove@gmx.de>
2025-04-13 16:50:47 +0200

    build: Bump version in package-lock.json; update .env.ts
    package-lock does not yet reflect the new version. The update
    affects .env.ts.
    
    Very first step to attack WEB-73

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '250403',
-    'hash': '66449454'
+    'version': '250413',
+    'hash': 'gff153bbf'
   },
   'allow_switching_backend_instance': true
 };
