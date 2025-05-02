2f41a6af7052ad571e13f6541e76edf042fb3474
gopi kishan <gkishan1kyt@gmail.com>
2025-05-02 16:41:16 +0530

    WEB-159 Replace references to MatLegacySnackBar and MatLegacySnackBarModule in Angular 15 project

--- src/app/system/manage-hooks/edit-hook/edit-hook.component.ts
+++ src/app/system/manage-hooks/edit-hook/edit-hook.component.ts
@@ -5,9 +5,9 @@ import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { NgModule } from '@angular/core';
-import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { ChangeDetectorRef } from '@angular/core';
 
