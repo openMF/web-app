84e552e39b405d3207e69cd6f0ccba2882ee3724
alberto-art3ch <alberto@art3ch.solutions>
2025-06-12 04:46:39 +0000

    Deploying to gh-pages from @ openMF/web-app@0a8aa1d5e93818e4d277152eba42a6d674c95d5b 🚀

--- browser/chunk-Y4A5RXFJ.js
+++
@@ -1,1 +0,0 @@
-import{C as r,Ef as a,F as c}from"./chunk-SR4VE5NL.js";var f=(()=>{class t{constructor(i){this.savingsService=i}resolve(i){let e=i.paramMap.get("savingAccountId");return this.savingsService.getSavingsAccountData(e)}static{this.\u0275fac=function(e){return new(e||t)(c(a))}}static{this.\u0275prov=r({token:t,factory:t.\u0275fac})}}return t})();export{f as a};
