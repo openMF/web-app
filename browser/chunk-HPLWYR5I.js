c63547f46eb56dd8befc98bb130e57f724f8a764
IOhacker <46640258+IOhacker@users.noreply.github.com>
2025-09-24 15:44:49 +0000

    Deploying to gh-pages from @ openMF/web-app@a4f8febcb3cc3381cadbb72c6cd23e3c54353c32 🚀

---
+++ browser/chunk-HPLWYR5I.js
@@ -0,0 +1,11 @@
+import{Fa as t,Ga as m,Ha as u,Pa as f,Sa as o,Ta as a,_ as n,ac as x,bd as E,ca as p,da as d,ja as c,lc as v,na as e,oa as i,oc as D,pc as M,qb as g,qc as b,rc as C,wc as S}from"./chunk-V3PGOA4N.js";var T=()=>({confirm:!0}),O=(()=>{class l{constructor(s){this.dialogRef=s}static{this.\u0275fac=function(r){return new(r||l)(p(v))}}static{this.\u0275cmp=d({type:l,selectors:[["mifosx-undo-transaction-dialog"]],decls:22,vars:14,consts:[["mat-dialog-title",""],["mat-dialog-content",""],["align","end"],["mat-raised-button","","mat-dialog-close",""],["mat-raised-button","","color","warn",3,"mat-dialog-close"]],template:function(r,h){r&1&&(e(0,"h1",0),t(1),o(2,"translate"),i(),t(3,`
+`),e(4,"div",1),t(5,`
+  `),e(6,"p"),t(7),o(8,"translate"),i(),t(9,`
+`),i(),t(10,`
+`),e(11,"mat-dialog-actions",2),t(12,`
+  `),e(13,"button",3),t(14),o(15,"translate"),i(),t(16,`
+  `),e(17,"button",4),t(18),o(19,"translate"),i(),t(20,`
+`),i(),t(21,`
+`)),r&2&&(n(),m(a(2,5,"labels.heading.Undo Transaction")),n(6),m(a(8,7,"labels.text.Are you sure you want to undo this transaction")),n(7),m(a(15,9,"labels.buttons.Cancel")),n(3),c("mat-dialog-close",f(13,T)),n(),u(`
+    `,a(19,11,"labels.buttons.Confirm"),`
+  `))},dependencies:[g,E,x,S,M,b,C,D],encapsulation:2})}}return l})();export{O as a};
