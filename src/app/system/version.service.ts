37d5b44be91e1f710e7fdf3053e5c5a73086c24e
Victor Romero <victor.romero@fintecheando.mx>
2025-12-01 21:14:49 -0600

    WEB-453: Remove fixed actuator endpoint

--- src/app/system/version.service.ts
+++ src/app/system/version.service.ts
@@ -5,13 +5,18 @@ import { HttpClient } from '@angular/common/http';
 /** rxjs Imports */
 import { Observable } from 'rxjs';
 
+/** Custom Services */
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class VersionService {
   constructor(private http: HttpClient) {}
 
+  private apiActuator = environment.apiActuator;
+
   getBackendInfo(): Observable<any> {
-    return this.http.get('/fineract-provider/actuator/info');
+    return this.http.get(this.apiActuator + '/actuator/info');
   }
 }
