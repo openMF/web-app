44c515f600de8917ee5ab395d513cd15eb5c7fd6
gkbishnoi07 <gkishan1kyt@gmail.com>
2026-01-07 05:10:41 +0000

    Deploying to gh-pages from @ openMF/web-app@451460d8e2871e1b873e835d6ea98058b05537da 🚀

--- browser/assets/env.template.js
+++ browser/assets/env.template.js
@@ -3,19 +3,24 @@
 
   // BackEnd Environment variables
   window['env']['fineractApiUrls'] = '$FINERACT_API_URLS';
+
   window['env']['fineractApiUrl'] = '$FINERACT_API_URL';
 
   window['env']['apiProvider'] = '$FINERACT_API_PROVIDER';
+
   window['env']['apiVersion'] = '$FINERACT_API_VERSION';
+
   window['env']['apiActuator'] = '$FINERACT_API_ACTUATOR';
 
   window['env']['fineractPlatformTenantId'] = '$FINERACT_PLATFORM_TENANT_IDENTIFIER';
+
   window['env']['fineractPlatformTenantIds'] = '$FINERACT_PLATFORM_TENANTS_IDENTIFIER';
 
   window['env']['tenantLogoUrl'] = '$TENANT_LOGO_URL';
 
   // Language Environment variables
   window['env']['defaultLanguage'] = '$MIFOS_DEFAULT_LANGUAGE';
+
   window['env']['supportedLanguages'] = '$MIFOS_SUPPORTED_LANGUAGES';
 
   window['env']['preloadClients'] = '$MIFOS_PRELOAD_CLIENTS';
