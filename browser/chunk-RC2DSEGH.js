8efceac317c38ed15e163df09ade88fbd16fd91f
gkbishnoi07 <gkishan1kyt@gmail.com>
2026-02-16 16:05:49 +0000

    Deploying to gh-pages from @ openMF/web-app@20072492e9c93b9bab190e5efe5a1f11e2459ea5 🚀

---
+++ browser/chunk-RC2DSEGH.js
@@ -0,0 +1,1 @@
+import{H as i,L as r,og as n}from"./chunk-7VWWNHGO.js";var f=(()=>{class t{constructor(){this.savingsService=r(n)}resolve(c){let e=c.paramMap.get("savingAccountId");return this.savingsService.getSavingsAccountData(e)}static{this.\u0275fac=function(e){return new(e||t)}}static{this.\u0275prov=i({token:t,factory:t.\u0275fac})}}return t})();export{f as a};
