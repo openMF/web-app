97e98dae71ec23ba7717db3540b53c5c656c140d
IOhacker <46640258+IOhacker@users.noreply.github.com>
2026-03-11 22:13:48 +0000

    Deploying to gh-pages from @ openMF/web-app@e7f6124a38c231ac7ab1811bc54cedf5e2adbd36 🚀

--- chunk-EL5VBK3N.js
+++
@@ -1,1 +0,0 @@
-import{H as i,L as r,rg as n}from"./chunk-JE2QKSXZ.js";var f=(()=>{class t{constructor(){this.savingsService=r(n)}resolve(c){let e=c.paramMap.get("savingAccountId");return this.savingsService.getSavingsAccountData(e)}static{this.\u0275fac=function(e){return new(e||t)}}static{this.\u0275prov=i({token:t,factory:t.\u0275fac})}}return t})();export{f as a};
