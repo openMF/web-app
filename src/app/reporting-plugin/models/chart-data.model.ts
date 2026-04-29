4d641ca5448adc927f5e7a4adf1fbf5d3e7d20a8
Sakshar Dhawan <sakshardhawanfzk@gmail.com>
2026-04-29 07:47:42 +0530

    WEB-678: Address CodeRabbit review comments on reporting plugin restructuring

--- src/app/reporting-plugin/models/chart-data.model.ts
+++ src/app/reporting-plugin/models/chart-data.model.ts
@@ -6,13 +6,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-/**
- * Copyright since 2025 Mifos Initiative
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+/** Chart Data Response Interface */
+export interface ChartDataResponse {
+  columnHeaders: { columnName: string }[];
+  data: { row: (string | number)[] }[];
+}
 
 /** Chart Data Model */
 export class ChartData {
@@ -21,10 +19,10 @@ export class ChartData {
   keys: string[];
   values: number[];
 
-  constructor(response: any) {
+  constructor(response: ChartDataResponse) {
     this.keysLabel = response.columnHeaders[0].columnName;
     this.valuesLabel = response.columnHeaders[1].columnName;
-    this.keys = response.data.map((object: any) => object.row[0]);
-    this.values = response.data.map((object: any) => object.row[1]);
+    this.keys = response.data.map((object: { row: (string | number)[] }) => object.row[0] as string);
+    this.values = response.data.map((object: { row: (string | number)[] }) => object.row[1] as number);
   }
 }
