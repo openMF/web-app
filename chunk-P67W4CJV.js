5d24c7eb5ec67d620c54fb88a274ed10326ad854
IOhacker <46640258+IOhacker@users.noreply.github.com>
2026-05-19 00:25:01 +0000

    Deploying to gh-pages from @ openMF/web-app@a2e4571a956ce52800c82bd7270ef78c5474a27f 🚀

---
+++ chunk-P67W4CJV.js
@@ -0,0 +1,1 @@
+import{a as i}from"./chunk-NZ6UK4ON.js";import{I as r,M as e}from"./chunk-ULUN5YEZ.js";var m=(()=>{class t{constructor(){this.organizationService=e(i)}resolve(){return this.organizationService.getOffices()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=r({token:t,factory:t.\u0275fac})}}return t})();export{m as a};
