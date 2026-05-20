b9c0c0b07b47c464ea79f6acb923ec1f4be2ebb3
alberto-art3ch <alberto@art3ch.solutions>
2026-05-20 14:32:28 +0000

    Deploying to gh-pages from @ openMF/web-app@6c754866cf919744229632d574972f78f55d314a 🚀

---
+++ chunk-AQ3NS6UL.js
@@ -0,0 +1,1 @@
+import{a as i}from"./chunk-6GL7NJOC.js";import{I as r,M as e}from"./chunk-LS23FQRH.js";var m=(()=>{class t{constructor(){this.organizationService=e(i)}resolve(){return this.organizationService.getOffices()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=r({token:t,factory:t.\u0275fac})}}return t})();export{m as a};
