0e1e3145fa02ac46ccdddf89b94148f4399f2c30
alberto-art3ch <alberto@art3ch.solutions>
2026-05-20 22:07:03 +0000

    Deploying to gh-pages from @ openMF/web-app@cd74f39fbf429331af3cf33566af545b3d979efe 🚀

--- chunk-NZXPLRGF.js
+++
@@ -1,1 +0,0 @@
-import{I as e,M as r,ne as i}from"./chunk-LS23FQRH.js";var m=(()=>{class t{constructor(){this.systemService=r(i)}resolve(){return this.systemService.getConfigurations()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})}}return t})();export{m as a};
