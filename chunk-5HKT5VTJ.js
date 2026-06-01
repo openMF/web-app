e96a1c028c539e5b8af6b425a8d111ce55a5b187
adamsaghy <adamsaghy@gmail.com>
2026-06-01 07:43:14 +0000

    Deploying to gh-pages from @ openMF/web-app@7db3ece0a6773ad7c788e8354631c7cac4d21b01 🚀

---
+++ chunk-5HKT5VTJ.js
@@ -0,0 +1,1 @@
+import{I as e,M as c,Pg as r}from"./chunk-VFUNW3BD.js";var u=(()=>{class t{constructor(){this.accountingService=c(r)}resolve(){return this.accountingService.getOffices()}static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275prov=e({token:t,factory:t.\u0275fac})}}return t})();export{u as a};
