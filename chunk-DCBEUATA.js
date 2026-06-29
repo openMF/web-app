0b865fa80cabeba19de589dcdae12b3a308c94c9
adamsaghy <adamsaghy@gmail.com>
2026-06-29 13:28:21 +0000

    Deploying to gh-pages from @ openMF/web-app@d57ce7539dcdb96df93a26fa9c325a247fe72be7 🚀

--- chunk-DCBEUATA.js
+++
@@ -1,1 +0,0 @@
-import{Fa as i}from"./chunk-SENTV2UL.js";import{ea as e,ja as r}from"./chunk-Q4ZLNCJ6.js";var m=(()=>{class t{constructor(){this.systemService=r(i)}resolve(){return this.systemService.getConfigurations()}static{this.\u0275fac=function(o){return new(o||t)}}static{this.\u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})}}return t})();export{m as a};
--- chunk-EG4ZE6RL.js
+++
@@ -1,1 +0,0 @@
-import{gd as r}from"./chunk-SENTV2UL.js";import{ea as c,ja as e}from"./chunk-Q4ZLNCJ6.js";var f=(()=>{class t{constructor(){this.accountingService=e(r)}resolve(){return this.accountingService.getGlAccounts()}static{this.\u0275fac=function(i){return new(i||t)}}static{this.\u0275prov=c({token:t,factory:t.\u0275fac})}}return t})();export{f as a};
