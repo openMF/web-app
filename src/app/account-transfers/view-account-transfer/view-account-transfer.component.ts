58ec7f155b0ed8b53517a1e2b82c0b1fae1ad7cd
Daniel Simiyu <daniel.simiyu@oneacrefund.org>
2026-07-07 15:03:32 +0300

    HQX-215 always show client loans button

--- src/app/account-transfers/view-account-transfer/view-account-transfer.component.ts
+++ src/app/account-transfers/view-account-transfer/view-account-transfer.component.ts
@@ -1,23 +1,67 @@
 /** Angular Imports */
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'mifosx-view-account-transfer',
   templateUrl: './view-account-transfer.component.html',
   styleUrls: ['./view-account-transfer.component.scss']
 })
-export class ViewAccountTransferComponent {
+export class ViewAccountTransferComponent implements OnDestroy {
 
   viewAccountTransferData: any;
+  private readonly destroy$ = new Subject<void>();
   /**
    * Retrieves the view account transfer data from `resolve`.
    * @param {ActivatedRoute} route Activated Route.
    */
-  constructor(private route: ActivatedRoute) {
-    this.route.data.subscribe((data: { viewAccountTransferData: any }) => {
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.data
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((data: { viewAccountTransferData: any }) => {
       this.viewAccountTransferData = data.viewAccountTransferData;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+    private getAccountUrl(accountTypeCode: string, clientId: string, accountId: string): string | null {
+    const base = `/clients/${clientId}`;
+    if (accountTypeCode === 'accountType.loan') {
+      return `${base}/loans-accounts/${accountId}/general`;
+    }
+    if (accountTypeCode === 'accountType.savings') {
+      return `${base}/savings-accounts/${accountId}/transactions`;
+    }
+    return null;
+  }
+
+  private navigateToAccount(accountTypeCode: string, clientId: string, accountId: string): void {
+    const url = this.getAccountUrl(accountTypeCode, clientId, accountId);
+    if (url) {
+      this.router.navigateByUrl(url);
+    } else {
+      console.warn(`Unsupported account type: ${accountTypeCode}`);
+    }
+  }
+
+  navigationURLToAccountFromOnViewTransferData(): void {
+    const { fromAccountType, fromClient, fromAccount } = this.viewAccountTransferData ?? {};
+    if (fromAccountType && fromClient && fromAccount) {
+      this.navigateToAccount(fromAccountType.code, fromClient.id, fromAccount.id);
+    }
+  }
+
+  navigationURLToAccountToOnViewTransferData(): void {
+    const { toAccountType, toClient, toAccount } = this.viewAccountTransferData ?? {};
+    if (toAccountType && toClient && toAccount) {
+      this.navigateToAccount(toAccountType.code, toClient.id, toAccount.id);
+    }
   }
 
 }
