ad7d2703a184ffe7f6b6127b1917ac9ffbc6ea05
Jamal <jamalkamaladdin@gmail.com>
2026-09-22 02:59:44 +0400

    feat(i18n): add Azerbaijani (az) locale

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -74,7 +74,7 @@ export const environment = {
   },
   defaultLanguage: loadedEnv['defaultLanguage'] || 'en-US',
   supportedLanguages:
-    loadedEnv['supportedLanguages'] || 'cs-CS,de-DE,en-US,es-MX,fr-FR,it-IT,ko-KO,lt-LT,lv-LV,ne-NE,pt-PT,sw-SW',
+    loadedEnv['supportedLanguages'] || 'az-AZ,cs-CS,de-DE,en-US,es-MX,fr-FR,it-IT,ko-KO,lt-LT,lv-LV,ne-NE,pt-PT,sw-SW',
   defaultFormatDate: loadedEnv['defaultFormatDate'] || '',
   defaultFormatDatetime: loadedEnv['defaultFormatDatetime'] || '',
   preloadClients: loadedEnv['preloadClients'] || true,
